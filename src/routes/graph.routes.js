949aded35fd4e3edbd695f9ad78ce82ce16b3b96
Dev Varshney <varshneydev365@gmail.com>
2026-03-18 10:39:06 +0530

    Update graph.routes.js

--- src/routes/graph.routes.js
+++ src/routes/graph.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { getGraph } from "../controllers/graph.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/:repoId", getGraph);
+// 🧬 Get Graph (protected)
+router.get("/:repoId", authMiddleware, getGraph);
 
 export default router;

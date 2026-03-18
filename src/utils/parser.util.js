76ba250700027ec4b9ea7168f83ee247056242f8
Dev Varshney <varshneydev365@gmail.com>
2026-03-18 11:15:39 +0530

    Update parser.util.js

--- src/utils/parser.util.js
+++ src/utils/parser.util.js
@@ -1,7 +1,22 @@
 export const extractDependencies = (pkg) => {
-  const deps = pkg.dependencies || {};
-  return Object.keys(deps).map(name => ({
-    name,
-    version: deps[name]
-  }));
+  const allDeps = {
+    ...pkg.dependencies,
+    ...pkg.devDependencies
+  };
+
+  const result = [];
+
+  for (const name in allDeps) {
+    const version = allDeps[name];
+
+    result.push({
+      name,
+      version,
+      cleanVersion: version.replace(/[^0-9.]/g, ""), // 🔥 important
+      type: pkg.dependencies?.[name] ? "prod" : "dev",
+      parent: null // 🔥 future use (chain)
+    });
+  }
+
+  return result;
 };

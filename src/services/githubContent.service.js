93f54ccaedfa03c886993bf60072356cfccac10c
Dev Varshney <varshneydev365@gmail.com>
2026-03-23 08:53:42 +0530

    Create githubContent.service.js

---
+++ src/services/githubContent.service.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export const fetchFileFromGitHub = async (url, filePath, token) => {
+  try {
+    const parts = url.split("github.com/")[1].split("/");
+    const owner = parts[0];
+    const repo = parts[1];
+
+    const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${filePath}`;
+
+    const res = await axios.get(apiUrl, {
+      headers: {
+        Authorization: `token ${token}`,
+        Accept: "application/vnd.github+json",
+      },
+    });
+
+    const content = Buffer.from(res.data.content, "base64").toString("utf-8");
+
+    return JSON.parse(content);
+
+  } catch (err) {
+    console.log(`❌ Failed to fetch ${filePath}`);
+    return null;
+  }
+};
